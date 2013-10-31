dd469c32c1e1d258c2efa178a27cd36518b05afb
JTReed <reed.jacksont@gmail.com>
2013-10-31 17:45:21 -0500

    Cylinder.cpp updated - draws cylinder (almost) successfully

--- CS559_project2v2/Cylinder.cpp
+++ CS559_project2v2/Cylinder.cpp
@@ -9,7 +9,6 @@ Cylinder::Cylinder()
 
 bool Cylinder::Initialize(float radius, float height, int slices, int stacks) 
 {
-	Mesh flatMesh;
 	// Initialize a flat mesh - Mesh::Inizialize(slices, stacks + 2)
 	//  - stacks + 2 because we need an extra stack on top and bottom to form the flat bits of the cylinder
 	// Modify the points of that flat mesh by:
@@ -17,14 +16,38 @@ bool Cylinder::Initialize(float radius, float height, int slices, int stacks)
 	// - bringing the top and bottom together to form the flat sides
 	// Make sure everything is centered around the origin or it'll be hard to manage
 
-	//if(!flatMesh.Initialize(slices, stacks))
-	//{
+	BuildMesh(slices, stacks);
+	BuildShape(radius, height, slices, stacks);
+	if(!super::Initialize())
+	{
 		return false;
-	//}
+	}
 
 }
 
-
-Cylinder::~Cylinder(void)
+void Cylinder::BuildShape(float radius, float height, int slices, int stacks)
 {
+	mat4 mv(1.0);
+
+	// Loop through EACH INDEX.
+	for(int i = 0; i < (slices * stacks); i++)
+	{
+		int curr_slice = i % slices;
+		int curr_stack = i / slices;
+
+		this->vertex_list[i].position.x = 0;
+		this->vertex_list[i].position.y = curr_stack * (height / (float)stacks);
+		this->vertex_list[i].position.z = radius;
+
+		// Multiply the position by the modelview matrix to apply it to the current mv position
+		this->vertex_list[i].position = vec3(vec4(this->vertex_list[i].position, 1) * mv);
+
+		mv = rotate(mv, -float(360.0f / slices), vec3(0.0f, 1.0f, 0.0f));
+		glLoadMatrixf(value_ptr(mv));
+	}
+}
+
+Cylinder::~Cylinder()
+{
+
 }
--- CS559_project2v2/Cylinder.h
+++ CS559_project2v2/Cylinder.h
@@ -7,7 +7,9 @@ class Cylinder: public Mesh
 public:
 	Cylinder();
 	bool Initialize(float radius, float height, int slices, int stacks);
-	//virtual void Draw(const glm::mat4 & projection, glm::mat4 modelview, const glm::ivec2 & size, const float time = 0);
-	~Cylinder(void);
+	void BuildShape(float radius, float height, int slices, int stacks);
+	~Cylinder();
+private:
+	typedef Mesh super;
 };
 
