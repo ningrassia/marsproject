0efa12b89ac293d81068e8121361c3eca5ec63b9
JTReed <reed.jacksont@gmail.com>
2013-10-31 17:53:52 -0500

    Added color input to Cylinder::Initialize()

--- CS559_project2v2/Cylinder.cpp
+++ CS559_project2v2/Cylinder.cpp
@@ -7,7 +7,7 @@ Cylinder::Cylinder()
 {
 }
 
-bool Cylinder::Initialize(float radius, float height, int slices, int stacks) 
+bool Cylinder::Initialize(float radius, float height, int slices, int stacks, vec3 color) 
 {
 	// Initialize a flat mesh - Mesh::Inizialize(slices, stacks + 2)
 	//  - stacks + 2 because we need an extra stack on top and bottom to form the flat bits of the cylinder
@@ -16,7 +16,7 @@ bool Cylinder::Initialize(float radius, float height, int slices, int stacks)
 	// - bringing the top and bottom together to form the flat sides
 	// Make sure everything is centered around the origin or it'll be hard to manage
 
-	BuildMesh(slices, stacks);
+	BuildMesh(slices, stacks, color);
 	BuildShape(radius, height, slices, stacks);
 	if(!super::Initialize())
 	{
--- CS559_project2v2/Cylinder.h
+++ CS559_project2v2/Cylinder.h
@@ -6,7 +6,7 @@ class Cylinder: public Mesh
 {
 public:
 	Cylinder();
-	bool Initialize(float radius, float height, int slices, int stacks);
+	bool Initialize(float radius, float height, int slices, int stacks, vec3 color);
 	void BuildShape(float radius, float height, int slices, int stacks);
 	~Cylinder();
 private:
